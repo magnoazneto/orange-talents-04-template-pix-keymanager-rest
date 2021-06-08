e46d8503864e3c1d6c68282988a04f28d263d643
magnoazneto <magnoazneto@gmail.com>
2021-06-08 11:30:03 -0300

    [tests] adiciona testes para funcionalidade de busca por clientId

--- src/test/kotlin/br/com/zup/ot4/keymanager/KeyManagerControllerTest.kt
+++ src/test/kotlin/br/com/zup/ot4/keymanager/KeyManagerControllerTest.kt
@@ -5,7 +5,9 @@ import br.com.zup.ot4.keymanager.registry.KeyPostRequest
 import br.com.zup.ot4.keymanager.registry.KeyTypeRequest
 import br.com.zup.ot4.keymanager.remove.KeyDeleteRequest
 import br.com.zup.ot4.keymanager.search.PixKeyDetailsResponse
+import br.com.zup.ot4.keymanager.search.PixKeyInfo
 import br.com.zup.ot4.shared.grpc.GrpcClientFactory
+import com.google.protobuf.Timestamp
 import io.micronaut.context.annotation.Factory
 import io.micronaut.context.annotation.Replaces
 import io.micronaut.http.HttpRequest
@@ -20,6 +22,8 @@ import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.any
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 import java.util.*
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -65,7 +69,7 @@ internal class KeyManagerControllerTest(
     }
 
     @Test
-    internal fun `deve remover uma chave pix existente`() {
+    fun `deve remover uma chave pix existente`() {
 
         val responseGrpc = RemoveKeyResponse.newBuilder()
             .setSuccess(true)
@@ -80,8 +84,54 @@ internal class KeyManagerControllerTest(
     }
 
     @Test
-    internal fun `deve retornar chave pix por PixID e ClientId`() {
-        val responseGrpc = SearchKeyResponse.newBuilder()
+    fun `deve retornar chave pix por PixID e ClientId`() {
+        `when`(keyManagerStub.search(any(SearchKeyRequest::class.java))).thenReturn(responseKeyGrpc())
+
+        val request = HttpRequest.GET<Any>("/api/v1/clientes/${clientId}/pix?pixId=$pixId")
+        val response = client.toBlocking().exchange(request, PixKeyDetailsResponse::class.java)
+
+        assertEquals(HttpStatus.OK, response.status)
+        assertEquals(pixId, response.body()!!.pixId)
+        assertEquals("teste@gmail.com", response.body()!!.key)
+    }
+
+    @Test
+    fun `deve retornar todas as chaves pix por clientId`() {
+
+        val responseGrpc = SearchAllResponse.newBuilder()
+            .addKeys(pixDetails())
+            .addKeys(pixDetails())
+            .addKeys(pixDetails())
+            .build()
+
+        `when`(keyManagerStub.searchAll(any(SearchAllRequest::class.java))).thenReturn(responseGrpc)
+
+        val request = HttpRequest.GET<Any>("/api/v1/clientes/${clientId}/pix")
+        val response = client.toBlocking().exchange(request, List::class.java)
+
+        assertEquals(HttpStatus.OK, response.status)
+        assertEquals(3, response.body().size)
+    }
+
+    private fun pixDetails(): SearchAllResponse.PixDetails {
+        val createdAtInstant = LocalDateTime.now().toInstant(ZoneOffset.UTC)
+        return SearchAllResponse.PixDetails.newBuilder()
+            .setPixId(UUID.randomUUID().toString())
+            .setExternalClientId(clientId)
+            .setKeyType(KeyType.RANDOM_KEY)
+            .setKey(UUID.randomUUID().toString())
+            .setAccountType(AccountType.CONTA_CORRENTE)
+            .setCreatedAt(
+                Timestamp.newBuilder()
+                    .setSeconds(createdAtInstant.epochSecond)
+                    .setNanos(createdAtInstant.nano)
+                    .build()
+            )
+            .build()
+    }
+
+    private fun responseKeyGrpc(): SearchKeyResponse? {
+        return SearchKeyResponse.newBuilder()
             .setExternalClientId(clientId)
             .setPixId(pixId)
             .setKeyType(KeyType.EMAIL)
@@ -94,15 +144,6 @@ internal class KeyManagerControllerTest(
                 .setAccountNumber("5432")
                 .setAccountType(AccountType.CONTA_CORRENTE).build())
             .build()
-
-        `when`(keyManagerStub.search(any(SearchKeyRequest::class.java))).thenReturn(responseGrpc)
-
-        val request = HttpRequest.GET<Any>("/api/v1/clientes/${clientId}/pix?pixId=$pixId")
-        val response = client.toBlocking().exchange(request, PixKeyDetailsResponse::class.java)
-
-        assertEquals(HttpStatus.OK, response.status)
-        assertEquals(pixId, response.body()!!.pixId)
-        assertEquals("teste@gmail.com", response.body()!!.key)
     }
 
     @Factory
